f862b8c1bd606ddeb9a7baa31ad383a790b4e444
Nick Eminizer <nick.eminizer@gmail.com>
2018-10-19 11:46:10 -0400

    After Higgs Combine presentation 10/17/2018
    bugfix for name of top tagging efficiency, added  "autoMCstats" line (commented out). Next changes will hopefully be for separated JEC factors

--- python/fit.py
+++ python/fit.py
@@ -228,7 +228,7 @@ class Fit(object) :
 		for line in template_file.readlines() :
 			#print line #DEBUG
 			#exclude/skip a couple lines specifically (namely top tagging efficiency if there are no top tags)
-			sys_to_skip = ['ttag_eff_weight'] if topology=='t3' else []
+			sys_to_skip = ['ttag_eff_weight_r'] if topology=='t3' else []
 			#sys_to_skip = ['JES']#,'top_pt_re_weight']#,'btag_eff_weight_b','btag_eff_weight_r','el_trig_eff_weight_b','el_trig_eff_weight_r']
 			#ignore top pt reweighting if it's already there in the templates
 			#sys_to_skip.append('top_pt_re_weight')
@@ -236,7 +236,7 @@ class Fit(object) :
 			if self._nojec :
 				sys_to_skip += ['JES','JER']
 			if self._noss :
-				sys_to_skip += ['pileup_weight',rep_data['lt']+'_trig_eff_weight_'+rep_data['tr'],rep_data['lt']+'_ID_weight',rep_data['lt']+'_iso_weight','btag_eff_weight','ren_scale_weight','fact_scale_weight','pdfas_weight','top_pt_re_weight']
+				sys_to_skip += ['pileup_weight',rep_data['lt']+'_trig_eff_weight_'+rep_data['tr'],rep_data['lt']+'_ID_weight',rep_data['lt']+'_iso_weight','btag_eff_weight_flavb_'+rep_data['tr'],'btag_eff_weight_flavc_'+rep_data['tr'],'btag_eff_weight_light_'+rep_data['tr'],'ttag_eff_weight_'+rep_data['tr'],'ren_scale_weight','fact_scale_weight','comb_scale_weight','pdfas_weight','top_pt_re_weight']
 			#print 'line split = %s'%(line.split()) #DEBUG
 			if line.split()[0]%rep_data in sys_to_skip :
 				continue
@@ -301,6 +301,8 @@ class Fit(object) :
 			for line in rate_params_lines :
 				#print 'new line: %s \n'%(line%rep_data) #DEBUG
 				outfile.write((line%rep_data)+'\n')
+		#add the line for autoMCstats
+		#outfile.write('* autoMCStats 1. 1')
 		temp_file.Close()
 		outfile.close()
 
